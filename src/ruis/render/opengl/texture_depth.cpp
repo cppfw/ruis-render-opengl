76509f0abc4706510ca25526fdf7b8eec9354e6c
Ivan Gagis <igagis@gmail.com>
2024-05-08 14:02:53 +0300

    stuff

---
+++ src/ruis/render/opengl/texture_depth.cpp
@@ -0,0 +1,9 @@
+#include "texture_depth.hpp"
+
+using namespace ruis::render::opengl;
+
+texture_depth::texture_depth(r4::vector2<uint32_t> dims) :
+	ruis::render::texture_depth(dims)
+{
+	// TODO:
+}
--- src/ruis/render/opengl/texture_depth.hpp
+++ src/ruis/render/opengl/texture_depth.hpp
@@ -1,8 +1,24 @@
 #pragma once
 
+#include <ruis/render/texture_depth.hpp>
+
+#include "opengl_texture.hpp"
+
 namespace ruis::render::opengl {
 
-class texture_depth : public ruis::render::texture_depth
-{};
+class texture_depth :
+	public opengl_texture, //
+	public ruis::render::texture_depth
+{
+	texture_depth(r4::vector2<uint32_t> dims);
+
+	texture_depth(const texture_depth&) = delete;
+	texture_depth& operator=(const texture_depth&) = delete;
+
+	texture_depth(texture_depth&&) = delete;
+	texture_depth& operator=(texture_depth&&) = delete;
+
+	~texture_depth() = default;
+};
 
 } // namespace ruis::render::opengl
