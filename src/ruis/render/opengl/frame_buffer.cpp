d666bfc92a21f83180e16ec6d089a230020013ad
Ivan Gagis <igagis@gmail.com>
2024-05-08 10:26:29 +0300

    stuff

--- src/ruis/render/opengl/frame_buffer.cpp
+++ src/ruis/render/opengl/frame_buffer.cpp
@@ -44,9 +44,9 @@ frame_buffer::frame_buffer(utki::shared_ref<ruis::render::texture_2d> color) :
 	glBindFramebuffer(GL_FRAMEBUFFER, this->fbo);
 	assert_opengl_no_error();
 
-	ASSERT(dynamic_cast<texture_2d*>(&this->color.get()))
+	ASSERT(dynamic_cast<texture_2d*>(this->color.get()))
 	// NOLINTNEXTLINE(cppcoreguidelines-pro-type-static-cast-downcast)
-	auto& tex = static_cast<texture_2d&>(this->color.get());
+	auto& tex = static_cast<texture_2d&>(*this->color);
 
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tex.tex, 0);
 	assert_opengl_no_error();
