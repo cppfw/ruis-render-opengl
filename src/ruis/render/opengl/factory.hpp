e847fb80db483306e3185da5037002684fadb4ba
Ivan Gagis <igagis@gmail.com>
2024-07-30 23:59:11 +0300

    format

--- src/ruis/render/opengl/factory.hpp
+++ src/ruis/render/opengl/factory.hpp
@@ -54,7 +54,8 @@ public:
 		texture_2d_parameters params
 	) override;
 
-	utki::shared_ref<ruis::render::texture_depth> create_texture_depth(rasterimage::dimensioned::dimensions_type dims
+	utki::shared_ref<ruis::render::texture_depth> create_texture_depth(
+		rasterimage::dimensioned::dimensions_type dims
 	) override;
 
 	utki::shared_ref<ruis::render::texture_cube> create_texture_cube(
@@ -66,11 +67,14 @@ public:
 		rasterimage::image_variant&& negative_z
 	) override;
 
-	utki::shared_ref<ruis::render::vertex_buffer> create_vertex_buffer(utki::span<const r4::vector4<float>> vertices
+	utki::shared_ref<ruis::render::vertex_buffer> create_vertex_buffer(
+		utki::span<const r4::vector4<float>> vertices
 	) override;
-	utki::shared_ref<ruis::render::vertex_buffer> create_vertex_buffer(utki::span<const r4::vector3<float>> vertices
+	utki::shared_ref<ruis::render::vertex_buffer> create_vertex_buffer(
+		utki::span<const r4::vector3<float>> vertices
 	) override;
-	utki::shared_ref<ruis::render::vertex_buffer> create_vertex_buffer(utki::span<const r4::vector2<float>> vertices
+	utki::shared_ref<ruis::render::vertex_buffer> create_vertex_buffer(
+		utki::span<const r4::vector2<float>> vertices
 	) override;
 	utki::shared_ref<ruis::render::vertex_buffer> create_vertex_buffer(utki::span<const float> vertices) override;
 	utki::shared_ref<ruis::render::index_buffer> create_index_buffer(utki::span<const uint16_t> indices) override;
