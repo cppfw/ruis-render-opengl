59c4365ba0a88c69e3e7f24744e529764e366fd1
Ivan Gagis <igagis@gmail.com>
2025-03-10 14:41:32 +0200

    factory->context

--- src/ruis/render/opengl/texture_2d.hpp
+++ src/ruis/render/opengl/texture_2d.hpp
@@ -38,7 +38,7 @@ public:
 		rasterimage::format type,
 		rasterimage::dimensioned::dimensions_type dims,
 		utki::span<const uint8_t> data,
-		ruis::render::factory::texture_2d_parameters params
+		ruis::render::context::texture_2d_parameters params
 	);
 
 	texture_2d(const texture_2d&) = delete;
